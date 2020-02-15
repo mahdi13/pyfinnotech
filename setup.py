550166813b66e0c968255e221506da70afe0998a
Mahdi Aali <mahdi13.1373@gmail.com>
2020-02-16 02:35:25 +0300

    Fix travis config

--- setup.py
+++ setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
 dependencies = ['requests']
+test_dependencies = ['nose']
 
 setup(
     name="pyfinnotech",
     version='0.2.0',
     author="mahdi13",
+    tests_require=test_dependencies,
+    extras_require={'test': test_dependencies},
     install_requires=dependencies,
     packages=find_packages(),
     test_suite="pyfinnotech.tests"
