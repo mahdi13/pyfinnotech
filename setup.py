e9ecfd0e017a72071e0e3d3badd4077e86f49e8d
Mahdi Aali <mahdi13.1373@gmail.com>
2020-10-18 22:01:27 +0300

    Upgrade version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ test_dependencies = ['nose', 'codecov']
 
 setup(
     name="pyfinnotech",
-    version='0.2.12',
+    version='0.2.13',
     author="mahdi13",
     tests_require=test_dependencies,
     extras_require={'test': test_dependencies},
